05b58f1ad68a51d3f088e1045d94cc96862ccf8a
makoto-developer <72484465+makoto-developer@users.noreply.github.com>
2022-09-11 13:00:19 +0900

    eslintのバージョンをダウングレード

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,40 +1,41 @@
+const [never, warn, error] = ['never', 'warn', 'error']
+
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended'
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'overrides': [],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      error,
+      2
+      , {'SwitchCase': 2}],
+    'linebreak-style': [
+      error,
+      'unix'
     ],
-    'overrides': [
+    'quotes': [
+      error,
+      'single'
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react',
-        '@typescript-eslint'
+    'semi': [
+      error,
+      never
     ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
-    }
+  }
 }
