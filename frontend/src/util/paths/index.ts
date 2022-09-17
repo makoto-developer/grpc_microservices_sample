c8ec0ecaa670e3f880fb1091303455e7a0c7b8ce
makoto-developer <72484465+makoto-developer@users.noreply.github.com>
2022-09-17 21:36:13 +0900

    update

--- frontend/src/util/paths/index.ts
+++ frontend/src/util/paths/index.ts
@@ -1,6 +1,6 @@
 export const PATHS = {
   TOP: '/',
-  ACCOUNT_LIST: '/account',
-  ACCOUNT_DETAIL: '/account/detail',
-  SETTINGS: '/settings'
+  SHOP_LIST: '/shop',
+  SHOP_DETAIL: '/shop/detail',
+  SETTINGS: '/settings',
 } as const
