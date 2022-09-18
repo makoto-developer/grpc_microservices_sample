daa270e1bc4c52731b1394a29b54b317a1588a11
makoto-developer <72484465+makoto-developer@users.noreply.github.com>
2022-09-18 10:21:26 +0900

    format axios client

--- frontend/src/util/api/index.ts
+++ frontend/src/util/api/index.ts
@@ -7,8 +7,10 @@ class ApiClient {
 
   constructor() {
     const backendBaseUrl = process.env.BACKEND_BASE_URL
+
     if (backendBaseUrl === undefined) {
       console.error('backend base url is not found.')
+
       return
     }
     ApiClient.backendBaseUrl = backendBaseUrl
@@ -16,30 +18,28 @@ class ApiClient {
     ApiClient.init()
   }
 
-  private static init = (token?: string) => {
+  private static init = () => {
     ApiClient.axios = ApiClient.createAxiosInstance(this.backendBaseUrl)
   }
 
   private static initIfNot = () => {
-    if (!ApiClient.axios) {
-      ApiClient.init()
-    }
+    if (!ApiClient.axios) ApiClient.init()
   }
 
   private static createAxiosInstance = (baseURL: string) => {
     if (!baseURL) throw new Error('base url is required.')
     const instance = axios
       .create({
-        baseURL: baseURL,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        timeout: 15_000,
+        baseURL:baseURL,
+        headers:{'Content-Type': 'application/json'},
+        timeout:15_000,
       })
+
     axios.interceptors.response.use(
       (config: any) => this.requestSuccess(config), // FIXME anyを削りたい...
       (config: any) => this.requestFailure(config), // FIXME anyを削りたい...
     )
+
     return instance
   }
 
@@ -53,45 +53,55 @@ class ApiClient {
 
   async get<Response>({url}: { url: string }) {
     ApiClient.initIfNot()
+
     try {
       return await ApiClient.axios.get<Response>(url)
-    } catch (error: unknown) {
+    }
+    catch (error: unknown) {
       throw new Error('get method cause error:')
     }
   }
 
   async post<Request, Response>({url, data}: { url: string, data: Request }) {
     ApiClient.initIfNot()
+
     try {
       return await ApiClient.axios.post<Request, Response>(url, data)
-    } catch (error: unknown) {
+    }
+    catch (error: unknown) {
       throw new Error('post method cause error:')
     }
   }
 
   async delete<Response>({url}: { url: string }) {
     ApiClient.initIfNot()
+
     try {
       return await ApiClient.axios.delete<Response>(url)
-    } catch (error: unknown) {
+    }
+    catch (error: unknown) {
       throw new Error('delete method cause error:')
     }
   }
 
   async patch<Request, Response>({url, data}: { url: string, data: Request }) {
     ApiClient.initIfNot()
+
     try {
       return await ApiClient.axios.patch<Response>(url, data)
-    } catch (error: unknown) {
+    }
+    catch (error: unknown) {
       throw new Error('patch method cause error:')
     }
   }
 
   async put<Request, Response>({url, data}: { url: string, data: Request }) {
     ApiClient.initIfNot()
+
     try {
       return await ApiClient.axios.put<Response>(url, data)
-    } catch (error: unknown) {
+    }
+    catch (error: unknown) {
       throw new Error('put method cause error:')
     }
   }
