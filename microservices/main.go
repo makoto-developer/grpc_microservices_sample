d4fd85355c3444b0d5b1025e3a3ad40f670ef0c5
makoto-developer <72484465+makoto-developer@users.noreply.github.com>
2022-10-03 23:52:00 +0900

    wip

--- microservices/main.go
+++ microservices/main.go
@@ -1,23 +1,14 @@
 package main
 import (
-    "fmt"
+    "github.com/gin-contrib/cors"
     "github.com/gin-gonic/gin"
 )
-type PrintJob struct {
-    JobId int `json:"jobId" binding:"required,gte=10000"`
-    Pages int `json:"pages" binding:"required,gte=1,lte=100"`
-}
 
 func main() {
     router := gin.Default()
-    router.POST("/print", func(c *gin.Context) {
-        var p PrintJob
-        if err := c.ShouldBindJSON(&p); err != nil {
-            c.JSON(400, gin.H{"error": "Invalid input!"})
-            return
-        }
-        c.JSON(200, gin.H{"message":
-        fmt.Sprintf("PrintJob #%v started!", p.JobId)})
+    router.Use(cors.Default())
+    router.GET("/", func(c *gin.Context) {
+        c.JSON(200, gin.H{"message": "CORS works!"})
     })
     router.Run(":5000")
 }
